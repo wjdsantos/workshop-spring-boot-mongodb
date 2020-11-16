d68c4c01efaf517ca8577ff2cce605576817d4ab
wjdsantos <walter4asantos@gmail.com>
2020-11-16 12:19:20 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.educandoweb.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.UserDTO;
 import com.educandoweb.workshopmongo.services.UserService;
 
 @RestController
@@ -19,11 +21,14 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 //		User maria = new User("1", "Maria Brown", "maria@gmail.com");
 //		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		List<User> list = service.findAll();
 //		list.addAll(Arrays.asList(maria, alex));
-		return ResponseEntity.ok().body(list);
+		List<User> list = service.findAll(); //Agora buscando do banco MongoDB
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());  //Refatorando o métdo findAll - aula347
+//		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDto);
+		
 	}
 }
